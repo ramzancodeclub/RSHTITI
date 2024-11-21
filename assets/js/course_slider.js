72b1dc2bb16bdc371a304704a2f7c13c05b68383
Ramzan <mramzanstv@gmail.com>
2024-11-21 09:55:15 +0500

    change location of index file and fix some accessibility issue. remove unwanted code from course and team slider js files.

--- assets/js/course_slider.js
+++ assets/js/course_slider.js
@@ -1,8 +1,6 @@
 $('.course-cards-container').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 1000,
     arrows: true,
     responsive: [
         {
